c9a3048f00affaebc29de2a1af7bfd10cc1ac7ea
Lessllie Real <rlessllie@yahoo.com>
2019-11-12 16:33:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,45 @@
 # code here!
+
+class School
+  
+  attr_accessor :roster
+  
+  hash = {}
+  hash["new_key"] = []
+  hash["new key"] << "new_value_for_value_array"
+  
+  def initialize(name)
+    @name = name
+    
+  end 
+  
+  def name
+    @name
+  end 
+  
+  
+  
+  def sort 
+  end 
+  
+  
+  
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+  end 
+  
+  def grade
+  end 
+  
+  
+  
+end
+
+school = School.new("Bayside High School") 
+school.roster
+p school 
+
+school.add_student("Zach Morris", 9)
+school.roster
+hash
